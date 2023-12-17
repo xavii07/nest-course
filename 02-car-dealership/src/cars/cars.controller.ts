90675477e3285eb6e27a4f6e96afeb330b5ea910
xavii07 <xaviergaspa123@gmail.com>
2023-12-17 16:59:29 -0500

    feat controler

---
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.getAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.getOneById(id);
+  }
+}
