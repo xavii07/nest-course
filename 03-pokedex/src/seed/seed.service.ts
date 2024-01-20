3e713a668076f071b6579ef285742be116c5bf53
xavii07 <xaviergaspa123@gmail.com>
2024-01-19 23:51:21 -0500

    feat adapter axios

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
-
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); //delete * from pokemons
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=5`,
     );
 
