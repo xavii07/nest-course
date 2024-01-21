fcef0938ef7574ff99c1c1e2c4ac3df9736f4fd3
xavii07 <xaviergaspa123@gmail.com>
2024-01-20 23:25:55 -0500

    feat coneccion a base de datos typeorm

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), //para poder usar las variables de entorno
+    TypeOrmModule.forRoot({
+      //para configurar la conexion a la base de datos
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, //todas las entidades se cargan automaticamentez
+      synchronize: true, //si hay cambios en el modelo, se sincroniza con la base de datos solo en dev mode
+    }),
+  ],
 })
 export class AppModule {}
