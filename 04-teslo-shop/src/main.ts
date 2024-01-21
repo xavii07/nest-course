01af453a03f4887bac234189299e7a9077382270
xavii07 <xaviergaspa123@gmail.com>
2024-01-21 10:57:58 -0500

    feat entity and dto

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api'); //agregar prefix a url de todos os endpoints
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
