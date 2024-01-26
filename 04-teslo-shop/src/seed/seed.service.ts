d81a36d126357611814af4127cf08917e67c6b06
xavii07 <xaviergaspa123@gmail.com>
2024-01-25 23:44:57 -0500

    feat seed data

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -13,6 +14,16 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productService.deleteAllProducts();
+
+    const products = initialData.products;
+    const promiseProducts = [];
+
+    products.forEach((product) => {
+      promiseProducts.push(this.productService.create(product));
+    });
+
+    await Promise.all(promiseProducts);
+
     return true;
   }
 }
